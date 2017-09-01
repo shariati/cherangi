08e3af1123f6a2887a499458f71b8d72231fc2ea
Amin Shariati <amin.shariati@accenture.com>
2017-09-01 12:44:13 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cherangi
 A small javascript library that finds an exact or similar color name.
+
+
+const cherangi = require('./index.js');
---
+++ cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'use strict';
+const pkg = require('./package.json');
+const cherangi = require('./');
+
+require('taketalk')({
+  init(input, options) {
+    console.log(cherangi(input, options));
+  },
+  help() {
+    console.log([
+      '',
+      '  ' + pkg.description,
+      '',
+      '  Usage',
+      '    cherangi <string>',
+      '    echo <string> | cherangi',
+      '',
+      '  Example',
+      '    cherangi "7CB9E8"',
+      maskinput('7CB9E8')
+    ].join('\n'));
+  },
+  version: pkg.version
+});
