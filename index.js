7907165bf68d92a70d04e540e7e403ce193b5182
Amin Shariati <amin.shariati@gmail.com>
2021-10-20 21:13:47 +0800

    Fixed styling issue and bumped version

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ function hexToRGB (hex) {
 
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
 
-  return result ? {
-    R: parseInt(result[1], 16),
-    G: parseInt(result[2], 16),
-    B: parseInt(result[3], 16)
-  } : {}
+  return result
+    ? {
+        R: parseInt(result[1], 16),
+        G: parseInt(result[2], 16),
+        B: parseInt(result[3], 16)
+      }
+    : {}
 }
 
 /*
