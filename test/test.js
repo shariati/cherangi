682f1d5733b39b5801e2e6b841ada44c9b4547fe
Amin Shariati <amin.shariati@gmail.com>
2021-10-20 21:27:12 +0800

    Added new test cases and updated color-names package

--- test/test.js
+++ test/test.js
@@ -3,22 +3,47 @@
 const assert = require('assert')
 const cherangi = require('../')
 
-describe('cherangi', () => {
-  it('should return Hickory Cliff', done => {
+describe('Testing color codes various format', () => {
+  it('An input of 7C6E6D, should return Hickory Cliff', done => {
     const expected = cherangi('7C6E6D').name
     assert.strict.equal('Hickory Cliff', expected)
     done()
   })
-  it('should return Hickory Cliff', done => {
+  it('An input of CdCDcD, should return Compact Disc Grey', done => {
+    const expected = cherangi('CdCDcD').name
+    assert.strict.equal('Compact Disc Grey', expected)
+    done()
+  })
+  it('An input of ddd, should return Steam', done => {
+    const expected = cherangi('ddd').name
+    assert.strict.equal('Steam', expected)
+    done()
+  })
+})
+describe('Testing few set of color codes in hex value', () => {
+  it('An input of #7C6E6D, should return Hickory Cliff', done => {
     const expected = cherangi('#7C6E6D').name
     assert.strict.equal('Hickory Cliff', expected)
     done()
   })
-  it('should return Meadow Violet', done => {
+  it('An input of #72497D, should return Lilac Violet', done => {
     const expected = cherangi('#72497D').name
-    assert.strict.equal('Meadow Violet', expected)
+    assert.strict.equal('Lilac Violet', expected)
     done()
   })
+  it('An input of #CDCDCD, should return Compact Disc Grey', done => {
+    const expected = cherangi('#CDCDCD').name
+    assert.strict.equal('Compact Disc Grey', expected)
+    done()
+  })
+  it('An input of #72497D, should return Lilac Violet', done => {
+    const expected = cherangi('#72497D').name
+    assert.strict.equal('Lilac Violet', expected)
+    done()
+  })
+})
+
+describe('Testing invalid inputs', () => {
   it('should return Black on invalid entry', done => {
     const expected = cherangi('bvcds').name
     assert.strict.equal('Black', expected)
